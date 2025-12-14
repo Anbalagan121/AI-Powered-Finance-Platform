8e228ec338b127f151463cf27dd0b43c7d3649ac
Anbalagan K <anbuk6722@gmail.com>
2025-12-14 08:00:15 -0800

    superBase Connect the project

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,5 +1,12 @@
-import Image from "next/image"
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <Button variant="destructive"> Subscribe to RoadsideCoder</ Button >;
+  return (
+    <div className="mt-40 flex justify-center">
+      <Button variant="destructive">
+        Subscribe to RoadsideCoder
+      </Button>
+    </div>
+  );
 }
--- my-app/components/Header.jsx
+++ my-app/components/Header.jsx
@@ -1,19 +1,67 @@
 "use client";
 
 import React from "react";
-import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+import {
+    SignedIn,
+    SignedOut,
+    UserButton,
+    SignInButton,
+} from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { LayoutDashboard, PenBox } from "lucide-react";
 
 const Header = () => {
     return (
-        <div>
-            <SignedOut>
-                <SignInButton />
-            </SignedOut>
+        <header className="fixed top-0 left-0 w-full bg-white shadow z-50 border-b">
+            <nav className="flex items-center justify-between px-6 py-3">
+                {/* Logo */}
+                <Link href="/">
+                    <Image
+                        src="/logo.png"
+                        alt="money image"
+                        height={60}
+                        width={200}
+                        className="h-12 w-auto object-contain"
+                    />
+                </Link>
 
-            <SignedIn>
-                <UserButton />
-            </SignedIn>
-        </div>
+                {/* Auth Buttons */}
+                <div className="flex items-center space-x-4">
+                    <SignedIn>
+                        <Link href={"/dashboard"} className="text-gray-600 hover:text-blue-600 flex item-center gap-2">
+                            <Button variant="outline">
+                                <LayoutDashboard size={18} />
+                                <span className="hidden md:inline">Dashboard</span>
+                            </Button>
+
+                        </Link>
+                        <Link href={"/transaction/create"}>
+                            <Button className="flex item-center gap-2">
+                                <PenBox size={18} />
+                                <span className="hidden md:inline">Add Transaction</span>
+                            </Button>
+
+                        </Link>
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton
+                            mode="modal"
+                            forceRedirectUrl="/dashboard"
+                        >
+                            <Button variant="outline">Login</Button>
+                        </SignInButton>
+                    </SignedOut>
+
+                    <SignedIn>
+                        <UserButton appearance={{
+
+                        }} />
+                    </SignedIn>
+                </div>
+            </nav>
+        </header>
     );
 };
 
